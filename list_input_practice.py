a0681efe83794d567ae14104c31b8e94626f3f28
venkyy22 <103706951+venkyy22@users.noreply.github.com>
2026-05-28 18:31:27 +0530

    Add input collection for city names to a list

---
+++ list_input_practice.py
@@ -0,0 +1,20 @@
+empty = []
+
+city1 = input("enter city name:")
+(empty.append(city1))
+
+city2 = input("enter city name:")
+(empty.append(city2))
+
+city3 = input("enter city name:")
+(empty.append(city3))
+
+city4 = input("enter city name:")
+(empty.append(city4))
+
+city5 = input("enter city name:")
+(empty.append(city5))
+
+print(empty)
+
+print(city1)
